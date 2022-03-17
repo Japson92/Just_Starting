7688d90b4df886abb9abdc39cd1beba4f881d04a
japson92 <sebastianlefek92@gmail.com>
2022-03-17 22:39:26 +0100

    Dodanie startera aplikacji

--- Apka/App.java
+++ Apka/App.java
@@ -1,4 +1,11 @@
 package Apka;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
