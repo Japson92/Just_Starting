0e27a60b64dc9967192fdf2fcd40a6ac92f382ea
japson92 <sebastianlefek92@gmail.com>
2022-03-18 23:20:06 +0100

    Stworzenie gui

---
+++ Apka/TemplateControler.java
@@ -0,0 +1,49 @@
+package Apka;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TemplateControler {
+    @FXML
+    private Label label;
+    private int i = 0;
+
+    @FXML
+    public void print1(){
+        label.setText("1");
+    }
+    @FXML
+    public void print2(){
+        label.setText("2");
+    }
+    @FXML
+    public void print3(){
+        label.setText("3");
+    }
+    @FXML
+    public void print4(){
+        label.setText("4");
+    }
+    @FXML
+    public void print5(){
+        label.setText("5");
+    }
+    @FXML
+    public void print6(){
+        label.setText("6");
+    }
+    @FXML
+    public void print7(){
+        label.setText("7");
+    }@FXML
+    public void print8(){
+        label.setText("8");
+    }@FXML
+    public void print9(){
+        label.setText("9");
+    }
+    @FXML
+    public void sum(){
+
+    }
+}
