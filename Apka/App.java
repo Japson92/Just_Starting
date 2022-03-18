c0e08bfe8d13b8052fbaa3d4bcb6e5860f9823d7
japson92 <sebastianlefek92@gmail.com>
2022-03-18 23:52:35 +0100

    dopiska

--- Apka/App.java
+++ Apka/App.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Template.fxml"));
@@ -15,5 +17,32 @@ public class App extends Application {
         Scene scene = new Scene(layout);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
+    }
+
+    public enum OperatorModes {
+        add, minus, multiply, divide
+    }
+
+    private Double num1;
+    private Double num2;
+    private OperatorModes mode = OperatorModes.add;
+
+    private Double calculate() {
+        if (mode == OperatorModes.add) {
+            return num1 + num2;
+        }
+        if (mode == OperatorModes.minus) {
+            return num1 - num2;
+        }
+        if (mode == OperatorModes.multiply) {
+            return num1 * num2;
+        }
+        if (mode == OperatorModes.divide) {
+            return num1 / num2;
+        }
+
+        return null;
     }
 }
